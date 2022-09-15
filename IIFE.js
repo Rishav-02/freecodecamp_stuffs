35690353e9a3815ba16b836583cbe1d408f0fce4
Rishav <rishavsinha380@gmail.com>
2022-09-15 12:51:58 +0530

    Immediately invoked function

---
+++ IIFE.js
@@ -0,0 +1,14 @@
+//Understand the Immediately Invoked Function Expression (IIFE)
+
+//Syntax:-
+
+// (function () {
+//     console.log("Chirp, chirp!");
+//   })();
+
+
+(function () {
+  console.log("A cozy nest is ready");
+})();
+
+// makeNest();
