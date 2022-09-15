2e0ee2f3eb0b235758c1e1e21e5a4a8811e56e61
Rishav <rishavsinha380@gmail.com>
2022-09-15 12:53:21 +0530

    Use Object.create method

---
+++ Inherit_Behaviors_from_a_Supertype.js
@@ -0,0 +1,18 @@
+//Inherit Behaviors from a Supertype
+
+//Question:-Object.create(obj) creates a new object, and sets obj as the new object's prototype. Recall that the prototype is like the "recipe" for creating an object. By setting the prototype of animal to be the prototype of Animal, you are effectively giving the animal instance the same "recipe" as any other instance of Animal.
+
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck=Object.create(Animal.prototype); // Change this line
+let beagle=Object.create(Animal.prototype); // Change this line
